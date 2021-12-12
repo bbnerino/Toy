7e36456098c8cac2beed547ee9833cd577944b5f
임현홍 <rednerino@naver.com>
2021-12-12 12:05:24 +0900

    react hooks

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,10 +1,19 @@
+import { useState } from 'react';
 import './App.css';
 import Counter from './Components/Counter';
+import Info from './Components/Info';
+import Average from './Components/Average';
 
 const App = () =>{
+  const [visible,setVisible] = useState(false)
   return (
     <div className="App">
       <Counter/>
+      <button 
+        onClick={()=>{setVisible(!visible)}}
+      >{visible ? '숨기기':'보이기'}</button>
+      {visible &&  <Info/>}
+      <Average/>
     </div>
   )
 }
