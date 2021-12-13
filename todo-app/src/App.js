a606326d26a926b308095caa0cb22d797c372044
임현홍 <rednerino@naver.com>
2021-12-13 16:22:07 +0900

    todo 최적화, ready to routerApp

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -2,42 +2,48 @@ import './App.css';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
-import React,{useState,useCallback,useRef} from 'react'
+import React,{useReducer,useCallback,useRef} from 'react'
+
+function createBulkTodos(){
+  const array = []
+  for (let i=1; i<=2500;i++){
+    array.push({
+      id:i,
+      text:`할 일${i}`,
+      checked:false
+    })
+  }
+  return array
+}
+function todoReducer(todos,action){
+  switch (action.type){
+    case 'INSERT':
+      return todos.concat(action.todo)
+    case 'REMOVE':
+      return todos.filter(todo=>todo.id!==action.id)
+    case 'TOGGLE':
+      return todos.map(todo=>
+        todo.id ===action.id ? {...todo,checked:!todo.checked}: todo)
+    default:
+      return todos
+    }
+}
 
 const App = ()=>{
   
-  const [todos,setTodos] = useState([
-    {
-      id:1,
-      text:'리액트기초 알아보기',
-      checked:true
-    },
-    {
-      id:2,
-      text:'컴포넌트 스타일링하기',
-      checked:true
-    },
-    {
-      id:3,
-      text:'일정 관리 앱 만들기',
-      checked:false
-    },
-  ])
+  const [todos,dispatch] = useReducer(todoReducer,undefined,createBulkTodos)
+  const nextId =useRef(2501)
+
   const onRemove = useCallback(
     id =>{
-      setTodos(todos.filter(todo=> todo.id!== id))
-    },
-    [todos]
-  )
+      dispatch({type:'REMOVE',id})
+    },[])
+
   const onToggle = useCallback(
     id=>{
-      setTodos(
-        todos.map(todo=>
-          todo.id === id ? {...todo,checked:!todo.checked} : todo),
-      )
-    },[todos]
+      dispatch({type:'TOGGLE',id})
+    },[]
   )
-  const nextId = useRef(4)
   const onInsert = useCallback(
     text=>{
       const todo = {
@@ -45,10 +51,10 @@ const App = ()=>{
         text,
         checked:false
       };
-      setTodos(todos.concat(todo))
+      dispatch({type:'INSERT',todo})
       nextId.current+=1;
     },
-    [todos]
+    []
   )
   return (
     <div>
