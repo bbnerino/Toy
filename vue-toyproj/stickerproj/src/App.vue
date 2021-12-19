4490d449d5b63d29ba2007ad7aa51af14547d74f
임현홍 <rednerino@naver.com>
2021-12-20 00:30:26 +0900

    sticker api success

--- vue-toyproj/stickerproj/src/App.vue
+++ vue-toyproj/stickerproj/src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="home">
-    <p>여기가 홈페이지입니다.</p>
-
+    <button @click="getSticker">가져오기</button>
+    <div v-if="view===true" style="display:flex"  >
+      <div v-for="pack in packageList" :key="pack.packageId">
+        {{pack.packageName}}
+        <img :src="`${pack.packageImg}`" alt="">
+      </div>
+      
+    </div>
   </div>
 </template>
 
@@ -10,21 +16,29 @@ import axios from "axios"
 
 export default {
   name: 'Home',
+  data(){
+      return {
+      view:false,
+      packageList : Object,
+      }
+    },
   methods: {
-    getStiker : function () {
+    getSticker : function () {
+      this.view = true
       axios.get('https://messenger.stipop.io/v1/package?userId=xxx&pageNumber=1&country=kr', {
           headers: { apikey: '8e2b8a6e14a05c53f8b184edede34e8d' },
         })
         .then((response) => {
-          console.log(response.data)
+          console.log(response.data.body.packageList)
+          this.packageList = response.data.body.packageList
+          
         })
         .catch((error) => {
           console.warn(error);  
       })
     }
   },
-  created : function () {
-    this.getStiker()
-  }
+
+  
 }
 </script>
