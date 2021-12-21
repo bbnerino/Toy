b5346f7ad952de74423c471c423d67b9d5df6f99
임현홍 <rednerino@naver.com>
2021-12-21 23:48:56 +0900

    vue

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Toy
+
+# React
+
+# Vue.js
+
+# django
+
+를 이용한 여러 toyproj 제작 예정
+
+
+
---
+++ vue/vuestartapp/src/views/TheLotto.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>{{$route.params}}</h1>
+    <button @click="getLuckyNum">클릭</button>
+    <h2>{{selectedLuckyNum}}</h2>
+  </div>
+</template>
+
+<script>
+import _ from 'lodash'
+
+export default {
+  name:'TheLotto',
+  data:function(){
+    return {
+      selectedLuckyNum:[],
+    }
+  },
+  methods:{
+    getLuckyNum : function(){
+      const numbers = _.range(1,46)
+      this.selectedLuckyNum = _.sampleSize(numbers,this.$route.params.lottoNumber)
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
