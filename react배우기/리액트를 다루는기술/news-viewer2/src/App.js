7c6897400af1aafbf569694541b322bc1a270cf4
bbnerino <bbnerino@gmail.com>
2022-01-07 19:28:18 +0900

    api 사용법

---
+++ react배우기/리액트를 다루는기술/news-viewer2/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import {Route} from 'react-router-dom'
+import NewsPage from './pages/NewsPage'
+
+function App() {
+  
+  return (
+    <Route path="/:category?" component={NewsPage} />
+  );
+}
+
+export default App;
---
+++ react배우기/리액트를 다루는기술/news-viewer2/src/pages/NewsPage.js
@@ -0,0 +1,13 @@
+import Categories from "../components/Categories";
+import NewsList from "../components/NewsList";
+
+const NewsPages=({match}) => {
+  const category = match.params.category || 'all';
+  return (
+    <>
+      <Categories/>
+      <NewsList category={category}/>
+    </>
+  )
+}
+export default NewsPages;
