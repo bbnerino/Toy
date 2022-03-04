1f09d9cd3f5de415aa4da5ca2cf41a315e435ca7
bbnerino <bbnerino@gmail.com>
2022-03-05 02:13:51 +0900

    make game

---
+++ NFT/GAME/js/MainScene.js
@@ -0,0 +1,52 @@
+import Player from "./Player.js"
+export default class MainScene extends Phaser.Scene{
+  constructor(){
+    super("MainScene");
+
+  }
+  preload(){
+    Player.preload(this);
+    this.load.image('tiles','assets/images/RPG Nature Tileset.png')
+    this.load.tilemapTiledJSON('map','assets/images/map.json')
+  }
+  create(){
+    const map = this.make.tilemap({key:'map'})
+    const tileset = map.addTilesetImage('RPG Nature Tileset','tiles',32,32)//사이즈
+    const layer1 = map.createStaticLayer('Tile Layer 1',tileset,0,0);
+    const layer2 = map.createStaticLayer('Tile Layer 2',tileset,0,0);
+    layer1.setCollisionByProperty({collides:true});
+    this.matter.world.convertTilemapLayer(layer1);
+    this.player = new Player({
+      scene:this,
+      x:200,
+      y:200,
+      texture:'female',
+      frame:'townfolk_f_idle_1'}
+      );
+
+    let textPlayer = new Player({
+        scene:this,
+        x:100,
+        y:100,
+        texture:'female',
+        frame:'townfolk_f_idle_1'}
+        );
+        let textPlayer2 = new Player({
+          scene:this,
+          x:100,
+          y:100,
+          texture:'female',
+          frame:'townfolk_f_idle_1'}
+          );
+    this.player.inputKeys = this.input.keyboard.addKeys({
+      up : Phaser.Input.Keyboard.KeyCodes.W,
+      down : Phaser.Input.Keyboard.KeyCodes.S,
+      left : Phaser.Input.Keyboard.KeyCodes.A,
+      right : Phaser.Input.Keyboard.KeyCodes.D,
+    })
+
+  }
+  update(){
+    this.player.update();
+  }
+}
