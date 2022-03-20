d1b8384daa6f2a9c4a37f0f1b591a5aca789c07a
bbnerino <bbnerino@gmail.com>
2022-03-20 23:47:16 +0900

    myroom add

---
+++ myroom/client/src/index.tsx
@@ -0,0 +1,24 @@
+import 'regenerator-runtime/runtime'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { ThemeProvider } from '@mui/material/styles'
+
+import './index.scss'
+import './PhaserGame'
+import muiTheme from './MuiTheme'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import store from './stores'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <ThemeProvider theme={muiTheme}> 
+        <App />
+      </ThemeProvider>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
+reportWebVitals()
