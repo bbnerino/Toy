d3a7cbe3b2c0b0df00c8b26213f00131b0287c97
bbnerino <83864924+bbnerino@users.noreply.github.com>
2022-07-17 21:48:05 +0900

    router

--- 사전준비/type-redux/src/App.tsx
+++ 사전준비/type-redux/src/App.tsx
@@ -3,15 +3,18 @@ import {Provider} from 'react-redux'
 import Change from './Change';
 import Samsung from './Samsung';
 import { store } from './store/store';
-
+import {BrowserRouter,Route, Routes} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <Provider store={store}>
-        <h1>예시코드입니다.</h1>
-        <Samsung/>
-        <Change/>
-      </Provider>
+      <BrowserRouter>
+        <Provider store={store}>
+          <Routes>
+            <Route path='/' element={<h1>헤ㅇ이ㅣ잉</h1>}/>
+            <Route path='/samsung' element={<><Samsung/><Change/></>}/>
+          </Routes>
+        </Provider>
+      </BrowserRouter>
     </div>
   );
 }
